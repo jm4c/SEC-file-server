fd2e096aa8b95e30cd6535d613cae116adbce949
RicardoFonsecaSilva <ricardo.filipe.silva@hotmail.com>
2016-03-28 13:57:10 +0100

    First phase of eIDlib_PKCS11, added to fs-utils

---
+++ fs-utils/src/main/java/eIDlib_PKCS11/test.java
@@ -0,0 +1,29 @@
+package eIDlib_PKCS11;
+
+import java.nio.charset.Charset;
+import sun.security.pkcs11.wrapper.PKCS11;
+
+public class test {
+    
+    public static void main(String args[])  {
+        java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();       
+        try
+        {
+            eIDLib_PKCS11_test.initLib();    
+            PKCS11 pkcs11 = eIDLib_PKCS11_test.initPKCS11();
+            long p11_session = eIDLib_PKCS11_test.initSession(pkcs11);
+
+            // sign
+            System.out.println("            //sign");
+            byte[] signature = pkcs11.C_Sign(p11_session, "data".getBytes(Charset.forName("UTF-8")));
+            System.out.println("            //signature:"+encoder.encode(signature));
+            
+            eIDLib_PKCS11_test.closeSession();
+
+        }  catch (Throwable e)
+        {
+            System.out.println("[Catch] Exception: " + e.getMessage());
+        }
+    }
+    
+}
