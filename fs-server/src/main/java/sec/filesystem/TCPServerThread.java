729dc769d22531505ace01352d73ef201c1496b5
jm4c <joao.d.j.machado@gmail.com>
2016-05-09 18:07:50 +0100

    TCP Server, started switch-case for different methods

--- fs-server/src/main/java/sec/filesystem/TCPServerThread.java
+++ fs-server/src/main/java/sec/filesystem/TCPServerThread.java
@@ -1,12 +1,25 @@
 package sec.filesystem;
 
 
-import java.io.*;
+import exceptions.IDMismatchException;
+import exceptions.InvalidSignatureException;
+import exceptions.WrongHeaderSequenceException;
+import types.Data_t;
+import types.Id_t;
+import types.Pk_t;
+import types.Sig_t;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 
 class TCPServerThread {
     private Socket socket;
-
     TCPServerThread(Socket socket) {
         this.socket = socket;
     }
@@ -14,11 +27,50 @@ class TCPServerThread {
 
     void run() {
         try {
+            ImplementationBlockServer server = new ImplementationBlockServer();
             BufferedReader inFromClient =
                     new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream outToClient =
                     new DataOutputStream(socket.getOutputStream());
-            outToClient.writeBytes(inFromClient.readLine().toUpperCase()+"\n");
+            String inputString = inFromClient.readLine();
+            String[] input = inputString.split(":");
+            try {
+                Data_t data = null;
+                Sig_t signature = null;
+                Pk_t publicKey = null;
+                Id_t id = null;
+
+                switch (input[0]) {
+                    case "put_h":
+                        //TODO
+                        server.put_h(data);
+                        break;
+                    case "put_k":
+                        //TODO
+                        server.put_k(data, signature, publicKey);
+                        break;
+                    case "get":
+                        //TODO
+                        server.get(id);
+                        break;
+                    default:
+
+                }
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (IDMismatchException e) {
+                e.printStackTrace();
+            } catch (WrongHeaderSequenceException e) {
+                e.printStackTrace();
+            } catch (InvalidKeyException e) {
+                e.printStackTrace();
+            } catch (SignatureException e) {
+                e.printStackTrace();
+            } catch (InvalidSignatureException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
 
 
         } catch (IOException e) {
